ead2c4f8b6b34be6e4640c5c5c92b4c3cec69dd9
Brendon Jennett <64869447+vxp101@users.noreply.github.com>
2021-09-04 12:23:20 -0400

    Add files via upload

---
+++ SendToVxp101.py
@@ -0,0 +1,97 @@
+import os
+import pickle
+import sys
+import server
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+from base64 import urlsafe_b64decode, urlsafe_b64encode
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+from email.mime.image import MIMEImage
+from email.mime.audio import MIMEAudio
+from email.mime.base import MIMEBase
+from email.mime.multipart import MIMEMultipart
+from mimetypes import guess_type as guess_mime_type
+
+SCOPES = ['https://mail.google.com/']
+our_email = 'me'
+files = []
+server.add_to_files("quick.py")
+server.add_to_files("quickstart.py")
+
+print(server.read_files())
+
+def gmail_authenticate():
+    creds = None
+    if os.path.exists("C:/Users/SxySt.DESKTOP-0LPF0L1/Documents/SendToVxp101/dist/SendToVxp101/token.pickle"):
+        with open("C:/Users/SxySt.DESKTOP-0LPF0L1/Documents/SendToVxp101/dist/SendToVxp101/token.pickle", "rb") as token:
+            creds = pickle.load(token)
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file('C:/Users/SxySt.DESKTOP-0LPF0L1/Documents/SendToVxp101/dist/SendToVxp101/credentials.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+        with open("C:/Users/SxySt.DESKTOP-0LPF0L1/Documents/SendToVxp101/dist/SendToVxp101/token.pickle", "wb") as token:
+            pickle.dump(creds, token)
+    return build('gmail', 'v1', credentials=creds)
+
+service = gmail_authenticate()
+
+def add_attachment(message, filename):
+    content_type, encoding = guess_mime_type(filename)
+    if content_type is None or encoding is not None:
+        content_type = 'application/octet-stream'
+    main_type, sub_type = content_type.split('/', 1)
+    if main_type == 'text':
+        fp = open(filename, 'rb')
+        msg = MIMEText(fp.read().decode(), _subtype=sub_type)
+        fp.close()
+    elif main_type == 'image':
+        fp = open(filename, 'rb')
+        msg = MIMEImage(fp.read(), _subtype=sub_type)
+        fp.close()
+    elif main_type == 'audio':
+        fp = open(filename, 'rb')
+        msg = MIMEAudio(fp.read(), _subtype=sub_type)
+        fp.close()
+    else:
+        fp = open(filename, 'rb')
+        msg = MIMEBase(main_type, sub_type)
+        msg.set_payload(fp.read())
+        fp.close()
+    filename = os.path.basename(filename)
+    msg.add_header('Content-Disposition', 'attachment', filename=filename)
+    message.attach(msg)
+
+def build_message(destination, obj, body, attachments=[]):
+    if not attachments: 
+        message = MIMEText(body) 
+        message['to'] = destination
+        message['from'] = our_email
+        message['subject'] = obj
+    else:
+        message = MIMEMultipart()
+        message['to'] = destination
+        message['from'] = our_email
+        message['subject'] = obj
+        message.attach(MIMEText(body))
+        for filename in attachments:
+            add_attachment(message, filename)
+    return {'raw': urlsafe_b64encode(message.as_bytes()).decode()}
+
+def send_message(service, destination, obj, body, attachments=[]):
+    return service.users().messages().send(
+      userId="me",
+      body=build_message(destination, obj, body, attachments)
+    ).execute()
+
+for i in sys.argv:
+    if i != sys.argv[0]:
+        files.append(i)
+
+print(sys.argv, "System Arguments")
+print(files, "Files")
+send_message(service, "me", "From Computer", 
+                "Important File", [*files])
